1b7f312fb8d052b1d711f9b0b6f5d669b2950c91
Saundra Catalina <saundracatalina@gmail.com>
2021-05-14 13:38:44 -0600

    Add route for creating a vac req

--- holiday_plans_saundra/config/routes.rb
+++ holiday_plans_saundra/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :employees, only: [:show]
+      resources :employees, only: [:show] do
+        resources :vacation_requests, only: [:create]
+      end
     end
   end
 end
