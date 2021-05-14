7a547729457453d014bc4bfc4d4917fb04fb9ef3
Saundra Catalina <saundracatalina@gmail.com>
2021-05-14 12:10:56 -0600

    Add relationships and validations to model

--- holiday_plans_saundra/app/models/employee.rb
+++ holiday_plans_saundra/app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ApplicationRecord
-  belongs_to :manager_id
+  belongs_to :manager
+  has_many :vacation_requests
+  validates_presence_of :name,
+                        :vac_days_left
 end
