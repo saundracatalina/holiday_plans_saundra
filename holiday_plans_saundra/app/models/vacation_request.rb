20ed9fb7ba702219d7128e755a5d37ab9fd7b031
Saundra Catalina <saundracatalina@gmail.com>
2021-05-14 12:14:05 -0600

    Add relationships and validations to model

--- holiday_plans_saundra/app/models/vacation_request.rb
+++ holiday_plans_saundra/app/models/vacation_request.rb
@@ -1,3 +1,7 @@
 class VacationRequest < ApplicationRecord
   belongs_to :employee
+  validates_presence_of :status,
+                        :start_date,
+                        :end_date
+  # no validation for resolved by--it can be null if pending
 end
