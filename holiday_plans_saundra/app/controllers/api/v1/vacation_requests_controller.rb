4256daff150e548285465e4a4df12e17e282df0f
Saundra Catalina <saundracatalina@gmail.com>
2021-05-14 14:04:24 -0600

    Add action for index

--- holiday_plans_saundra/app/controllers/api/v1/vacation_requests_controller.rb
+++ holiday_plans_saundra/app/controllers/api/v1/vacation_requests_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::VacationRequestsController < ApplicationController
+  def index
+    employee = Employee.find(params[:employee_id])
+    render json: VacationRequestSerializer.new(employee.vacation_requests), status: 200
+  end
+
   def create
     vacation_request = VacationRequest.create(vacation_request_params)
     render json: VacationRequestSerializer.new(vacation_request), status: 201
